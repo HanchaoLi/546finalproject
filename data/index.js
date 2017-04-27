d6c30b574ebc52d5a2e6c4402f38429f745d0d05
房家豪 <ff_ti@boukagous-MacBook-Pro.local>
2017-04-27 12:20:59 -0400

    updated

--- data/index.js
+++
@@ -1,8 +0,0 @@
-const usersData = require("./users");
-const registerData = require("./register");
-const feedbackData = require("./feedback");
-module.exports = {
-    users:usersData,
-    register:registerData,
-    feedback:feedbackData
-};
